190c009117bbe4be00ddca9d7786f50cf740923f
Mac Signer <macsigner@gmx.ch>
2022-09-22 22:35:59 +0200

    feat: add structured page tree to page collection

--- node-scripts/page-collection.js
+++ node-scripts/page-collection.js
@@ -15,28 +15,26 @@ class PageCollection {
     }) {
         this._settings = options;
         this.pages = this.getPages(globExpression);
-
-        console.log(this.pages);
     }
 
     getPages(globExpression) {
         const files = glob.sync(globExpression);
+        const filePaths = files.map((filePath) => this._parsePath(filePath));
 
-        this.pages = files.map((filePath) => this._parsePath(filePath));
-
-        let pageCollection = [];
-        for(let page of this.pages) {
-            pageCollection.push(this._applyTreeStructure(page));
+        let pageCollection = {};
+        for (let page of filePaths) {
+            pageCollection = this._applyTreeStructure(page, pageCollection);
         }
 
-        return this.pages;
+        return pageCollection;
     }
 
     _parsePath(filePath) {
         let dir = path.dirname(filePath);
-        dir = dir.replace(`${this._settings.pageRoot}/`, '');
+        dir = dir.replace(`${this._settings.pageRoot}`, '');
 
         let directoryArray = dir.split('/');
+        directoryArray.shift();
         let file = path.parse(filePath);
         let url = filePath.replace(this._settings.pageRoot, '');
 
@@ -44,12 +42,35 @@ class PageCollection {
             url,
             file,
             parents: directoryArray,
-            parentsString: directoryArray.join(','),
         }
     }
 
-    _applyTreeStructure(item) {
-        return item;
+    _applyTreeStructure(item, obj = {}) {
+        if (!obj.pages) {
+            obj.pages = {};
+        }
+
+        if (item.parents.length === 0 || (item.parents.length === 1 && item.parents[0] === item.file.name)) {
+            delete item.parents;
+
+            item.type = 'page';
+
+            obj.pages[item.file.name] = obj.pages[item.file.name] ? obj.pages[item.file.name] : {};
+
+            obj.pages[item.file.name] = Object.assign(obj.pages[item.file.name], item);
+        } else {
+            let key = item.parents.shift();
+
+            obj.pages[key] = obj.pages[key] ? obj.pages[key] : {};
+
+            obj.pages[key] = Object.assign(obj.pages[key], this._applyTreeStructure(item, obj.pages[key]));
+        }
+
+        if (!item.type) {
+            item.type = 'folder';
+        }
+
+        return obj;
     }
 }
 
