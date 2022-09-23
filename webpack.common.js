01b05d1d24d3a898ada76ad9538b16e61812a008
Mac Signer <macsigner@gmx.ch>
2022-09-23 19:26:03 +0200

    feat: add check for additional scripts for pages

--- webpack.common.js
+++ webpack.common.js
@@ -1,17 +1,25 @@
+import * as fs from 'fs';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import {CleanWebpackPlugin} from 'clean-webpack-plugin';
 import PageCollection from './node-scripts/page-collection.js';
 
 const pages = new PageCollection('src/pages/**/*.html');
+const additionalScriptFiles = pages.getPageObjects().reduce((config, page) => {
+    let jsFilePath = `./src/js/pages${page.parentDirectoryPath}/${page.file.name}.js`;
+
+    if (fs.existsSync(jsFilePath)) {
+        let chunkName = `chunk${page.parentDirectoryPath}-${page.file.name}`;
+
+        config[chunkName] = jsFilePath;
+    }
+
+    return config;
+}, {});
 
 export default {
     entry: Object.assign({
         app: './src/js/app.js',
-    }, pages.reduce((config, page) => {
-        config[page] = `./src/js/pages/${page}.js`;
-
-        return config;
-    }, {})),
+    }, additionalScriptFiles),
     module: {
         rules: [
             {
@@ -29,13 +37,25 @@ export default {
     plugins: [
         new CleanWebpackPlugin(),
     ].concat(
-        pages.map(page => {
-            console.log(page);
+        pages.getPageObjects().map(page => {
+            let chunks = ['app'];
+            let chunkName = `chunk${page.parentDirectoryPath}-${page.file.name}`;
+
+            if (Object.keys(additionalScriptFiles).includes(chunkName)) {
+                chunks.push(chunkName);
+            }
+
+            let filename = `${page.file.name}.html`
+
+            if (page.parentDirectoryPath) {
+                filename = `${page.parentDirectoryPath}/${filename}`.substring(1);
+            }
+
             return new HtmlWebpackPlugin({
                 inject: true,
-                template: `./src/pages/${page}.html`,
-                filename: `${page}.html`,
-                chunks: ['app', page],
+                template: `./src/pages${page.parentDirectoryPath}/${page.file.name}.html`,
+                filename,
+                chunks,
             });
         })
     )
