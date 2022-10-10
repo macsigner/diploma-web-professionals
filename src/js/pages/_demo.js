66f7294b7c649dbb85b72c351784d6b0f5a44a33
Mac Signer <macsigner@gmx.ch>
2022-10-10 14:25:07 +0200

    feat: add demo page for testing

---
+++ src/js/pages/_demo.js
@@ -0,0 +1,11 @@
+import Modal from '../components/Modal.js';
+
+let generatedContentInput = document.querySelector('#modal-input-generated');
+
+document.querySelector('#modal-button-generated').addEventListener('click', () => {
+    let title = document.createElement('h1');
+
+    title.innerHTML = generatedContentInput.value;
+
+    new Modal(title);
+});
